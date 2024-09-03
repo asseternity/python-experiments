593315e593df05f1a83e15e9afa8129075e4f335
asseternity <nakupovasset@gmail.com>
2024-09-03 23:45:52 +0500

    feat: tkinter library continued

--- interface.py
+++ interface.py
@@ -1,13 +1,32 @@
 import tkinter as tk
 
+# Create the main window
 window = tk.Tk()
 window.title("Hello, World!")
 
-def handle_any_button_press(event):
+# Create a heading label
+heading = tk.Label(window, text="Welcome to my app!", font={"Arial", 24})
+heading.pack(pady=10)
+
+# Function to be called when the count button is clicked
+count = 0
+def handle_count_button_press(event):
+    global count
+    count += 1
+    button1.config(text=f"Counter {count} times")
+
+# Create the count button
+button1 = tk.Button(text="Counted 0 times")
+button1.bind("<Button-1>", handle_count_button_press)
+button1.pack(pady=10)
+
+# Function to be called when the close button is clicked
+def handle_close_button_press(event):
     window.destroy()
 
-button = tk.Button(text="Close the app")
-button.bind("<Button-1>", handle_any_button_press)
-button.pack()
+# Create the close button
+button2 = tk.Button(text="Close the app")
+button2.bind("<Button-1>", handle_close_button_press)
+button2.pack(pady=10)
 
 window.mainloop()
